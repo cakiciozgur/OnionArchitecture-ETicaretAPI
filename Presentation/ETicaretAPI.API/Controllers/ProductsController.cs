98be22970001bf92f3004c2ad3657987ed439a89
cakiciozgur <cakiciozgur@outlook.com>
2023-12-12 21:36:25 +0300

    product get by id test

--- Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
+++ Presentation/ETicaretAPI.API/Controllers/ProductsController.cs
@@ -1,4 +1,5 @@
 ﻿using ETicaretAPI.Application.Repositories;
+using ETicaretAPI.Domain.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -28,6 +29,14 @@ namespace ETicaretAPI.API.Controllers
             });
 
             var count = await _productWriteRepository.SaveAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(string id)
+        {
+            Product product =  await _productReadRepository.GetByIdAsync(id);
+
+            return Ok(product);
         }
     }
 }
