f5f37073221a37df2e33b63dd5ed417c365fc040
cakiciozgur <cakiciozgur@outlook.com>
2023-12-12 20:39:19 +0300

    generic repository pattern, iwrite repository implemantation.

--- Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
+++ Core/ETicaretAPI.Application/Repositories/IWriteRepository.cs
@@ -9,10 +9,12 @@ namespace ETicaretAPI.Application.Repositories
 {
     public interface IWriteRepository<T> : IRepository<T> where T : BaseEntity
     {
-        Task<bool> AddAsync(T model);
-        Task<bool> AddRangeAsync(List<T> model);
-        Task<bool> Remove(T model);
-        Task<bool> Remove(string id);
-        Task<bool> UpdateAsync(T model);
+        Task<bool> AddAsync(T datas);
+        Task<bool> AddRangeAsync(List<T> datas);
+        bool Remove(T model);
+        bool RemoveRange(List<T> datas);
+        Task<bool> RemoveAsync(string id);
+        bool Update(T model);
+        Task<int> SaveAsync(T model);
     }
 }
